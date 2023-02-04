347fd56780d3cb63e26036823ff4b630cd98ac2e
Alexander Spasov <alexander.r.spasov@gmail.com>
2023-02-04 17:16:28 +0200

    LastCommit

---
+++ src/UnitTree.java
@@ -0,0 +1,71 @@
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+import org.xml.sax.SAXException;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import java.io.IOException;
+import java.util.*;
+
+public class UnitTree {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        //The unitID of the root
+        String unitID = scanner.nextLine().trim();
+
+        //The java DOM parser provides the classes to read and write an XML file.
+        DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+
+        //A set for saving the visited nodes units from the tree and a level variable for printing the level of the current node.
+        int level = 0;
+        Set<String> visited = new HashSet<>();
+
+        try {
+            DocumentBuilder builder = factory.newDocumentBuilder();
+            Document document = builder.parse("content.xml");
+            document.getDocumentElement().normalize();
+
+            printUnitTree(unitID, document, visited, level);
+
+        } catch (ParserConfigurationException | SAXException | IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    //This method finds the needed unit element in the node and traverses through its requirements. The method is recursive in order to find all the required unit elements.
+    public static void printUnitTree(String NodeID, Document document, Set<String> visited, int level) {
+        NodeList list = document.getElementsByTagName("unit");
+
+        //This loop finds the element in the document
+        for (int i = 0; i < list.getLength(); i++) {
+            Node currentItem = list.item(i);
+
+            //This condition checks if the given id matches the unitID.
+            if (currentItem.getNodeType() == Node.ELEMENT_NODE && list.item(i).getAttributes().getNamedItem("id").getNodeValue().equals(NodeID)) {
+                //This loop prints a different output depending on the level the requirement is at.
+                for(int itr = 0; itr < level; itr++){
+                    System.out.print("--");
+                }
+                System.out.println(NodeID);
+                visited.add(NodeID);
+
+                //We traverse through the requirements of the current unit element.
+                Element eElement = (Element) currentItem;
+                for (int j = 0; j < ((Element) currentItem).getElementsByTagName("required").getLength(); j++) {
+                    String requirement = (eElement.getElementsByTagName("required").item(j).getAttributes().getNamedItem("name").getNodeValue());
+                    String requirementNamespace = (eElement.getElementsByTagName("required").item(j).getAttributes().getNamedItem("namespace").getNodeValue());
+
+                    //This condition checks if the requirement is already visited and if the requirements namespace and name are valid.
+                    if (!visited.contains(requirement) && !requirement.equals(NodeID) && (requirementNamespace.equals("org.eclipse.equinox.p2.iu") || requirementNamespace.equals("osgi.bundle"))) {
+                        //If such and element is found the process is repeated with and increased depth level.
+                        printUnitTree(requirement, document, visited, level + 1);
+                    }
+                }
+                break;
+            }
+        }
+    }
+}
